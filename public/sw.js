c87db473d4673b51093d2862335896b94f129408
JarodGabriel <jarodgabrielm@gmail.com>
2026-05-30 15:23:15 -0700

    Bump app shell cache

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "echolog-lucid-shell-v14";
+const CACHE_NAME = "echolog-lucid-shell-v15";
 const SHELL_ASSETS = ["/", "/manifest.webmanifest", "/icon.svg"];
 
 self.addEventListener("install", (event) => {
